cdb054d1798090dea0dd05ff9ffb0036399d0ffe
sanzhixiong1986 <57082213+sanzhixiong1986@users.noreply.github.com>
2022-03-05 23:59:01 +0400

    启动net和puremvc的框架

--- MyPrueMVC/Assets/scripts/StartGame.cs
+++ MyPrueMVC/Assets/scripts/StartGame.cs
@@ -8,33 +8,11 @@ using Newtonsoft.Json;
 
 public class StartGame : MonoBehaviour
 {
-    private WebSocket ws;
     // Start is called before the first frame update
     void Start()
     {
-        ws = new WebSocket("ws://localhost:6081");
-        ws.Connect();
-        ws.OnMessage += (sender, e) =>
-        {
-            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
-        };
+        new ApplicationFacade(this.gameObject);
+        this.gameObject.AddComponent<NetMgr>();
     }
-    private void Update()
-    {
-        if(ws == null)
-        {
-            return;
-        }
-
-        MyData myData = new MyData();
-        myData.Level = 1;
-        string json = JsonConvert.SerializeObject(myData);
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ws.Send(json);
-        }  
-    }   
 }
 
