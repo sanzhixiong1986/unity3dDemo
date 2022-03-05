a61896c4230f67d27f868c32273dc69fdbb99c9d
sanzhixiong1986 <57082213+sanzhixiong1986@users.noreply.github.com>
2022-03-05 23:09:34 +0400

    测试了websocket的json通讯

--- MyPrueMVC/Assets/scripts/StartGame.cs
+++ MyPrueMVC/Assets/scripts/StartGame.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using WebSocketSharp;
+
+using Newtonsoft.Json;
 
 public class StartGame : MonoBehaviour
 {
+    private WebSocket ws;
     // Start is called before the first frame update
     void Start()
     {
-        new ApplicationFacade(this.gameObject);   
+        ws = new WebSocket("ws://localhost:6081");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+        };
     }
+        private void Update()
+        {
+            if(ws == null)
+            {
+                return;
+            }
+
+            MyData myData = new MyData();
+            myData.Level = 1;
+            string json = JsonConvert.SerializeObject(myData);
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ws.Send(json);
+            }  
+        }   
 }
+
